aab424d8c41df4d2b9836c01fbcf55bfa88bb843
dongqui <kimwinner90@gmail.com>
2020-03-30 16:52:13 +0900

    set redux, saga, styled compo

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { store } from './store';
+import { createBrowserHistory } from 'history';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const history = createBrowserHistory();
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store.tsx
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import rootReducer from'./reducers/index';
+
+export const history = createHistory();
+export const store = createStore(rootReducer(history));
