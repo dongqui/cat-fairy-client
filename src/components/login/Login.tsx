1a7056d142b450933f9a4327abef02552b7aee66
dongqui <kimwinner90@gmail.com>
2020-04-16 19:04:04 +0900

    init login

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Login() {
+  
+  return (
+    <div>
+      <a
+          className="App-link"
+          href="https://github.com/login/oauth/authorize?client_id=5c82987314849c415fa5&scope=user"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+    </div>
+  );
+}
+
+export default Login;
+
+
