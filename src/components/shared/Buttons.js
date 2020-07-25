f6d2219aaa1872770bdf3061723401301742af99
dongqui <dongqui9@gmail.com>
2020-07-25 23:42:55 +0900

    add onclick, resolve classes error

--- src/components/shared/Buttons.js
+++ src/components/shared/Buttons.js
@@ -2,24 +2,15 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import React from 'react';
 
-const useStyles = makeStyles({
-  btnBase: {
-    width: '80%',
-  },
-});
-
 export function DefaultButton(props) {
-  const { text, variant, color, size, fullWidth } = props;
-  const { btnBase } = useStyles();
+  const { text, variant, color, size, fullWidth, onClick } = props;
   return (
     <Button
-      classes={{
-        btnBase
-      }}
       variant={variant}
       color={color}
       size={size}
       fullWidth={fullWidth}
+      onClick={onClick}
     >
       {text}
     </Button>
