628a677655ccde8fb8e791da3d033a4c8b567316
dongqui <dongqui9@gmail.com>
2020-08-17 23:48:31 +0900

    대략적인 Route 구성
    Intro, Main, Login 페이지로 크게 나뉨

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import NavBar from './navBar/NavBar';
 import Login from './login/Login';
+import Intro from './intro/Intro';
+import Main from './main/Main';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
     <div className="App">
       <NavBar/>
       <Router>
+        <Route exact path='/main' component={Main}/>
         <Route exact path='/login' component={Login}/>
+        <Route exact path='/intro' component={Intro}/>
       </Router>
     </div>
   );
