dfbdf1ac3628c4ecb2cdbddc0f9b1212852c3b5c
dongqui <dongqui9@gmail.com>
2020-08-25 23:46:07 +0900

    배경 추가

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import background from '../../asset/img/배경.png'
 
+const useStyles = makeStyles({
+  root: {
+    background: `url(${background}) no-repeat`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100vh',
+    margin: '0 auto',
+  },
+});
 function Main() {
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
 
+    </div>
+  )
 }
 
 export default Main;
