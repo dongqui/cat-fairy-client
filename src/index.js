2e88e6585385a70a2514397c7634caf9213e946c
dongqui <dongqui9@gmail.com>
2020-08-31 23:56:49 +0900

    set redux extension

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/index';
 import * as serviceWorker from './serviceWorker';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from "./store/saga";
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const middlewares = [sagaMiddleware];
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = devTools || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(...middlewares)
+  ),
+);
 sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
