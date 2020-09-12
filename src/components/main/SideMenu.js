b7a41c5ccf7d74af18bf2d26b2b2e03935a654e1
dongqui <dongqui9@gmail.com>
2020-09-12 23:44:03 +0900

    사이드바 추가

---
+++ src/components/main/SideMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import S from './styles';
+import { toggleSideBar } from '../../store/mian';
+
+function SideMenu() {
+
+  return (
+    <S.SidebarContainer>
+      Hello
+    </S.SidebarContainer>
+  );
+}
+
+export default SideMenu;
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,17 +1,23 @@
-import React, {useEffect} from 'react';
-import SelectCat from './SelectCat';
-import backgroundMain from '../../asset/img/배경.png'
-import { Background } from '../shared/index';
+import React, { useEffect } from 'react';
+import SelectCat from '../components/main/SelectCat';
+import SideMenu from '../components/main/SideMenu';
+import backgroundMain from '../asset/img/배경.png'
+import { Background } from '../components/shared';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Main() {
 
+  const dispatch = useDispatch();
+  const { isSelectCatOpen } = useSelector(state => state.main);
+
   useEffect(() => {
 
-  })
+  }, [])
 
-  return(
+  return (
     <Background url={backgroundMain}>
-      <SelectCat/>
+      <SideMenu/>
+      { isSelectCatOpen && <SelectCat/> }
     </Background>
   )
 }
