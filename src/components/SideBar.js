9a31da9f9469e32c1ece7092e13840400cb22538
dongqui <dongqui9@gmail.com>
2020-10-02 12:58:53 +0900

    사이드바 기본 스타일

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,14 +1,61 @@
 import React from 'react';
-import S from './styles';
-import { toggleSideBar } from '../store/mian';
-import cat_feet_img from '../asset/img/cat_feet.JPG';
+import styled from 'styled-components';
+import whiteCate from '../asset/img/흰냥.png'
 
 function SideBar() {
 
   return (
-    <S.SidebarContainer>
-    </S.SidebarContainer>
+    <S.Sidebar>
+      <S.SidebarItemList>
+        <S.SidebarItem>
+          <img src={whiteCate}/>
+        </S.SidebarItem>
+        <S.SidebarItem>
+          <img src={whiteCate}/>
+        </S.SidebarItem>
+        <S.SidebarItem>
+          <img src={whiteCate}/>
+        </S.SidebarItem>
+        <S.SidebarItem>
+          <img src={whiteCate}/>
+        </S.SidebarItem>
+        <S.SidebarItem>
+          <img src={whiteCate}/>
+        </S.SidebarItem>
+      </S.SidebarItemList>
+    </S.Sidebar>
   );
 }
+
+const Sidebar = styled.aside`
+  position: absolute;
+  right: 0;
+  top: 20%;
+  height: 60%;
+  width: 100px;
+`;
+
+const SidebarItemList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+const SidebarItem = styled.li`
+  list-style: none;  
+  > img {
+  width: 100%;
+  }
+`;
+
+const S = {
+  Sidebar,
+  SidebarItemList,
+  SidebarItem
+};
+
+
 
 export default SideBar;
