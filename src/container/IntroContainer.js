95e0b9a234a658b718bb9527b5da143652c14234
dongqui <dongqui9@gmail.com>
2020-10-12 21:16:50 +0900

    유저 체크 loginbridge로 이동

--- src/container/IntroContainer.js
+++ src/container/IntroContainer.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Intro from '../components/Intro';
 
 export function IntroContainer() {
-  const { user } = useSelector(state => state.auth);
 
   return (
-    <Intro user={user}/>
+    <Intro/>
   )
 }
