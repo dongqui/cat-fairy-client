363697266183a90dfbfa96e230e3dc6819dcb23b
dongqui <dongqui9@gmail.com>
2020-10-19 21:32:18 +0900

    commitHistory api url 수정

--- src/api/commitHistory.js
+++ src/api/commitHistory.js
@@ -2,7 +2,7 @@ import axios from './axios';
 
 export const getCommitHistoryApi = async (uid) => {
   try {
-    const response = await axios.get('/getCommitHistory', { params: { uid } });
+    const response = await axios.get('/updateCommitHistory', { params: { uid } });
     return response.data;
   } catch (e) {
     console.log(e);
