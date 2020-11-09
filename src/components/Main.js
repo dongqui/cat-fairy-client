c38fa73294fe7e0103dcd6acc6d6da4ebb6534c1
dongqui <dongqui9@gmail.com>
2020-11-09 23:31:32 +0900

    사이드 바 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import backgroundMain from '../asset/img/배경.png'
 import { Background } from '../common';
+import SideBar from './SideBar';
 
 function Main() {
   return (
     <Background url={backgroundMain}>
+      <SideBar/>
     </Background>
   )
 }
